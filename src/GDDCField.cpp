3da4df698fc8dcd0c7e9b8fa579b057faa08b481
Kylie Smith <kylie.smithy01@gmail.com>
2024-02-16 11:46:40 +1000

    Fixed up field packing/unpacking

--- src/GDDCField.cpp
+++ src/GDDCField.cpp
@@ -26,7 +26,7 @@ int GDDCField::get_number() const
  * Generates a datagram containing the message necessary to send an update for
  * the indicated distributed object from the client.
  */
-godot::Ref<Datagram> GDDCField::client_format_update( int do_id, godot::Array args )
+Ref<Datagram> GDDCField::client_format_update( int do_id, godot::Array args )
 {
     DCPacker packer;
 
@@ -35,13 +35,13 @@ godot::Ref<Datagram> GDDCField::client_format_update( int do_id, godot::Array ar
     packer.raw_pack_uint16( get_number() );
 
     packer.begin_pack( _dcField );
-    pack_args( packer, std::move( args ) );
+    pack_args( packer, args );
     if ( !packer.end_pack() )
     {
         return {};
     }
 
-    Ref<Datagram> dg;
+    Ref<Datagram> dg = memnew( Datagram );
     dg->SetBytes( reinterpret_cast<const uint8_t *>( packer.get_data() ), packer.get_length() );
 
     return dg;
@@ -51,8 +51,7 @@ godot::Ref<Datagram> GDDCField::client_format_update( int do_id, godot::Array ar
  * Generates a datagram containing the message necessary to send an update for
  * the indicated distributed object from the AI.
  */
-godot::Ref<Datagram> GDDCField::ai_format_update( int do_id, int to_id, int from_id,
-                                                  godot::Array args )
+Ref<Datagram> GDDCField::ai_format_update( int do_id, int to_id, int from_id, godot::Array args )
 {
     DCPacker packer;
 
@@ -70,7 +69,7 @@ godot::Ref<Datagram> GDDCField::ai_format_update( int do_id, int to_id, int from
         return {};
     }
 
-    Ref<Datagram> dg;
+    Ref<Datagram> dg = memnew( Datagram );
     dg->SetBytes( reinterpret_cast<const uint8_t *>( packer.get_data() ), packer.get_length() );
 
     return dg;
@@ -82,16 +81,15 @@ godot::Ref<Datagram> GDDCField::ai_format_update( int do_id, int to_id, int from
  */
 void GDDCField::receive_update( DCPacker &packer, godot::Array args ) const
 {
-    if ( _dcField->as_parameter() != nullptr )
-    {
-        // If it's a parameter-type field, just store a new value on the object.
-        unpack_args( packer, args );
-    }
+    // Unpack the arguments.
+    // This *may* result in a recursive call-chain while it unpacks data
+    // (if it's a more complex field type.)
+    unpack_args( packer, args );
 }
 
 bool GDDCField::pack_args( DCPacker &packer, godot::Array args ) const
 {
-    pack_object( packer, std::move( args ) );
+    pack_object( packer, args );
     if ( !packer.had_error() )
     {
         return true;
@@ -119,7 +117,7 @@ void GDDCField::pack_object( DCPacker &packer, godot::Array args ) const
     {
         case PT_int64:
         {
-            Variant arg = args.front();
+            Variant arg = args.pop_front();
             if ( arg.get_type() == godot::Variant::INT )
             {
                 packer.pack_int64( arg.operator int64_t() );
@@ -129,7 +127,7 @@ void GDDCField::pack_object( DCPacker &packer, godot::Array args ) const
         }
         case PT_uint64:
         {
-            Variant arg = args.front();
+            Variant arg = args.pop_front();
             if ( arg.get_type() == godot::Variant::INT )
             {
                 packer.pack_uint64( arg.operator uint64_t() );
@@ -139,7 +137,7 @@ void GDDCField::pack_object( DCPacker &packer, godot::Array args ) const
         }
         case PT_int:
         {
-            Variant arg = args.front();
+            Variant arg = args.pop_front();
             if ( arg.get_type() == godot::Variant::INT )
             {
                 packer.pack_int( arg.operator int32_t() );
@@ -149,7 +147,7 @@ void GDDCField::pack_object( DCPacker &packer, godot::Array args ) const
         }
         case PT_uint:
         {
-            Variant arg = args.front();
+            Variant arg = args.pop_front();
             if ( arg.get_type() == godot::Variant::INT )
             {
                 packer.pack_uint( arg.operator uint32_t() );
@@ -159,7 +157,7 @@ void GDDCField::pack_object( DCPacker &packer, godot::Array args ) const
         }
         case PT_double:
         {
-            Variant arg = args.front();
+            Variant arg = args.pop_front();
             if ( arg.get_type() == godot::Variant::FLOAT || arg.get_type() == godot::Variant::INT )
             {
                 packer.pack_double( arg.operator double() );
@@ -169,7 +167,7 @@ void GDDCField::pack_object( DCPacker &packer, godot::Array args ) const
         }
         case PT_string:
         {
-            Variant arg = args.front();
+            Variant arg = args.pop_front();
             if ( arg.get_type() == godot::Variant::STRING )
             {
                 packer.pack_string( std::string( arg.operator String().utf8().get_data() ) );
@@ -183,8 +181,28 @@ void GDDCField::pack_object( DCPacker &packer, godot::Array args ) const
             ERR_PRINT( "Blob packing not yet supported" );
             break;
         }
-        default:
+        case PT_array:
+        {
+            ERR_PRINT( "Array packing not yet supported" );
             break;
+        }
+        case PT_field:
+        {
+            packer.push();
+            pack_object( packer, args );
+            packer.pop();
+            break;
+        }
+        case PT_class:
+        {
+            ERR_PRINT( "Class packing not yet supported" );
+            break;
+        }
+        case PT_switch:
+        {
+            ERR_PRINT( "Switch packing not yet supported" );
+            break;
+        }
     }
 }
 
@@ -195,116 +213,73 @@ void GDDCField::unpack_object( DCPacker &packer, godot::Array args ) const
     switch ( pack_type )
     {
         case PT_invalid:
+        {
             args.append( Variant() );
             packer.unpack_skip();
             break;
-
+        }
         case PT_double:
         {
             double value = packer.unpack_double();
             args.append( value );
+            break;
         }
-        break;
-
         case PT_int:
         {
             int value = packer.unpack_int();
             args.append( value );
+            break;
         }
-        break;
-
         case PT_uint:
         {
             unsigned int value = packer.unpack_uint();
             args.append( value );
+            break;
         }
-        break;
-
         case PT_int64:
         {
             int64_t value = packer.unpack_int64();
             args.append( value );
+            break;
         }
-        break;
-
         case PT_uint64:
         {
             uint64_t value = packer.unpack_uint64();
             args.append( value );
+            break;
         }
-        break;
-
         case PT_blob:
         {
-            // TODO: Needs to a bytes-like type here.
+            // TODO: Needs to be a bytes-like type here.
             std::string str;
             packer.unpack_string( str );
             args.append( String( str.c_str() ) );
+            break;
         }
-        break;
-
         case PT_string:
         {
             std::string str;
             packer.unpack_string( str );
             args.append( String( str.c_str() ) );
+            break;
         }
-        break;
-
-            // TODO.
-            //        case PT_class:
-            //        {
-            //            const DCClassParameter *class_param =
-            //            _this->get_current_field()->as_class_parameter(); if ( class_param !=
-            //            nullptr )
-            //            {
-            //                const DCClass *dclass = class_param->get_class();
-            //                if ( invoke_extension( dclass ).has_class_def() )
-            //                {
-            //                    // If we know what kind of class object this is and it has a valid
-            //                    // constructor, create the class object instead of just a tuple.
-            //                    object = unpack_class_object( dclass );
-            //                    if ( object == nullptr )
-            //                    {
-            //                        std::cerr << "Unable to construct object of class " <<
-            //                        dclass->get_name()
-            //                                  << "\n";
-            //                    }
-            //                    else
-            //                    {
-            //                        break;
-            //                    }
-            //                }
-            //            }
-            //        }
-            // Fall through (if no constructor)
-
-            // If we don't know what kind of class object it is, or it doesn't have a
-            // constructor, fall through and make a tuple.
-            //        default:
-            //        {
-            //            // First, build up a list from the nested objects.
-            //            object = PyList_New( 0 );
-            //
-            //            _this->push();
-            //            while ( _this->more_nested_fields() )
-            //            {
-            //                PyObject *element = unpack_object();
-            //                PyList_Append( object, element );
-            //                Py_DECREF( element );
-            //            }
-            //            _this->pop();
-            //
-            //            if ( pack_type != PT_array )
-            //            {
-            //                // For these other kinds of objects, we'll convert the list into a
-            //                // tuple.
-            //                PyObject *tuple = PyList_AsTuple( object );
-            //                Py_DECREF( object );
-            //                object = tuple;
-            //            }
-            //        }
-            //        break;
+        case PT_class:
+        {
+            ERR_PRINT( "Class unpacking not implemented" );
+            break;
+        }
+        case PT_array:
+        case PT_field:
+        case PT_switch:
+        {
+            packer.push();
+            while ( packer.more_nested_fields() )
+            {
+                unpack_object( packer, args );
+            }
+            packer.pop();
+            break;
+        }
     }
 }
 
